a2ac853c2632a8e5f43e01cb55293622d0d25fd1
Ebad S <ebad@ebad.dev>
2026-03-12 12:18:18 -0500

    Routers, Handle and Customer Handlers

--- main.go
+++ main.go
@@ -5,19 +5,35 @@ import (
 	
 )
 
+func handlerReadiness(w http.ResponseWriter, r *http.Request){
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8",)
+	w.WriteHeader(200)
+	w.Write([]byte("OK"))
+
+}
+
 func main(){
 
-
-	ptr := http.NewServeMux()
-
+	// NewServeMux -> lookup table matching incoming request -> endpoint -> Handler
+	regisBook := http.NewServeMux()
+	// Handle -> expects: Handler Object, http.FilerServer returns one of those handler objects
+	// http.FileServer(http.Dir(".") -> This gives us a fileserver that can handle requests 
+	// Why StripPrefix -> /app/ doesnt exist in our root 
+	regisBook.Handle("/app/", http.StripPrefix("/app/", http.FileServer(http.Dir("."))))
+	
+	// HandleFunc -> expects the name of a fucntion in this case the handlerReadiness 
+	// We give HandleFunc our func signature so the server can later call this function with the writer and request
+	regisBook.HandleFunc("/healthz", handlerReadiness)
+	
 	server := http.Server{
 		Addr: ":8080",
-		Handler: ptr,
+		Handler: regisBook,
 	}
 
-
-	server.ListenAndServe()
 	
+	server.ListenAndServe()
+
 
 
 
---
+++ readme.md
@@ -0,0 +1,18 @@
+# HTTP Server Architecture
+
+## 1. TCP Listener
+
+## 2. Connection Handling
+
+## 3. Parsing the Request
+
+## 4. Router
+
+## 5. Handlers
+
+## 6. Response
+
+## 7. Middleware
+
+
+
