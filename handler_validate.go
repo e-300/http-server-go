e6f7956ec5dbdeceeac8b85b5bf302a627e7dc73
Ebad S <ebad@ebad.dev>
2026-03-18 16:59:38 -0500

    handler_validate with profanity check

---
+++ handler_validate.go
@@ -0,0 +1,82 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"log"
+	"io"
+	"strings"
+)
+
+// Profane Word Func
+func profaneWords(r string) string{
+	profaneWords := map[string]string{
+		"kerfuffle": "****",
+		"sharbert": "****",
+		"fornax": "****",
+	
+	}
+	res := ""
+	wordList := strings.Split(r, " ")
+	for _, word := range wordList{
+		elem, ok := profaneWords[string(strings.ToLower(word))]
+		if ok{
+			res += elem
+		}else{
+			res += string(word)
+		}
+		res += " "
+	}
+	res = strings.TrimRight(res, " ")
+	return res
+}
+
+func handlerChirpsValidate(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	type requestBody struct{
+		Msg string `json:"body"`
+	}
+
+	type responseBody struct {
+		Valid bool `json:"valid"`
+		CleanedBody string `json:"cleaned_body"`
+	}
+	
+	// Reading raw JSON bytes from request 
+	dat, err := io.ReadAll(r.Body)
+	if err != nil{
+		err := respondWithError(w, 500, "Something went wrong")
+		if err != nil{
+			log.Println(err)
+		}
+		return 
+	}
+	
+	// Raw bytes Mapped into request struct 
+	params := requestBody{}
+	err = json.Unmarshal(dat, &params)
+	if err != nil{
+		err := respondWithError(w, 500, "Something went wrong")
+		if err != nil{
+			log.Println(err)
+		}
+		return 
+	}
+
+	// Business Logic
+
+	requestMsg := params.Msg
+	if len(requestMsg) > 140{
+		err := respondWithError(w, 400, "Chirp is too long")
+		if err != nil{
+			log.Println(err)
+		}
+		return 
+	}
+	res := profaneWords(requestMsg)
+	respondWithJSON(w, 200, responseBody{
+		Valid: true,
+		CleanedBody: res,
+	})
+
+}
