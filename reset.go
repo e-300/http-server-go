2a3525bb7b620b327b7549b7f0f32cc8349ee392
Ebad S <ebad@ebad.dev>
2026-03-18 17:01:28 -0500

    .

---
+++ reset.go
@@ -0,0 +1,8 @@
+package main
+import "net/http"
+
+func (cfg *apiConfig) resetHits(w http.ResponseWriter, r *http.Request){
+	cfg.fileserverHits.Store(0)
+	w.WriteHeader(200)
+    w.Write([]byte("Hits reset to 0"))
+}
