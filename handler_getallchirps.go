dab7cd591c592eb34375127e65563e6bd4d24fcd
Ebad S <ebad@ebad.dev>
2026-04-01 19:27:32 -0500

    new handler to get all chirps or posts in post db

---
+++ handler_getallchirps.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	//"encoding/json"
+	"net/http"
+)
+
+func (cfg *apiConfig) getAllChirps(w http.ResponseWriter, r *http.Request){
+	defer r.Body.Close()
+
+	
+	allChirps, err := cfg.db.GetAllChirps(r.Context())
+	if err != nil{
+		respondWithError(w, http.StatusInternalServerError, "Couldn't get all chirps from database")
+		return
+	}
+
+	responseChirps := []Chirp{}
+
+	for _, chirp := range(allChirps){
+		responseChirps = append(responseChirps,Chirp{
+			ID: chirp.ID,
+			CreatedAt: chirp.CreatedAt,
+			UpdatedAt: chirp.UpdatedAt,	
+			Body: chirp.Body,     	
+			UserID: chirp.UserID,
+		})
+	}
+
+	// marshalledChirps, err := json.Marshal(responseChirps)
+	// if err != nil{
+	// 	respondWithError(w, http.StatusInternalServerError, "coudnt marshall chirps")
+	// 	return		
+	// }
+	
+	respondWithJSON(w, http.StatusOK, responseChirps)
+}
