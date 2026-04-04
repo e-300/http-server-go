545aaf717bd48e6cbb050773ae66b8a896206845
Ebad S <ebad@ebad.dev>
2026-04-04 01:09:33 -0500

    file paths cleaned up

---
+++ internal/auth/jwt.go
@@ -0,0 +1,8 @@
+package auth
+
+import (
+	"github.com/google/uuid"
+	"time"
+)
+
+func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error)
---
+++ internal/database/sql/schema/002_posts.sql
@@ -0,0 +1,14 @@
+-- +goose Up 
+CREATE TABLE posts(
+    id UUID Primary Key,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    body TEXT NOT NULL,
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE
+);
+
+
+-- +goose Down
+DROP TABLE  posts;
+
+
