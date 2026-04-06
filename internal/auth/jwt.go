ea4d16ff4855c201282e582137f1962881091805
Ebad S <ebad@ebad.dev>
2026-04-05 20:09:03 -0500

    ADDED: makejwt function to auth package

--- internal/auth/jwt.go
+++ internal/auth/jwt.go
@@ -1,10 +1,29 @@
 package auth
 
 import (
-	"github.com/google/uuid"
 	"time"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
+
 func MakeJWT(userID uuid.UUID, tokenSecret string, expiresIn time.Duration) (string, error){
-	return "",nil
+	
+	now := time.Now().UTC()
+	
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.RegisteredClaims{
+		Issuer: "chirpy-access",
+		IssuedAt: jwt.NewNumericDate(now),
+		ExpiresAt:  jwt.NewNumericDate(now.Add(expiresIn)),
+		Subject: userID.String(),
+
+	})
+
+	bytetoken := []byte(tokenSecret)
+	signedtoken, err := token.SignedString(bytetoken)
+	if err != nil{
+		return "", err
+	}
+
+	return signedtoken, nil
 }
