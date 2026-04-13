61f1f5ded2fce24e8e924e40d34856c05db56ee4
Ebad S <ebad@ebad.dev>
2026-04-13 12:45:24 -0500

    refresh token goose migration table created && makerefreshtoken hex encoding function finished

---
+++ internal/auth/refresh_token.go
@@ -0,0 +1,21 @@
+package auth
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+)
+
+func MakeRefreshToken() (string, error){
+	// byte slice of len 32 
+	key := make([]byte, 32)
+	// read random bytes into key in place
+	_, err := rand.Read(key)
+	if err != nil{
+		return "", err
+	}
+
+	hexKey := hex.EncodeToString(key)
+
+	return hexKey, nil
+}
+
