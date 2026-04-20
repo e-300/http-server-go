e5aca92ad925fdef1aab2fd37afd84755084505a
Ebad S <ebad@ebad.dev>
2026-04-20 14:46:18 -0500

    .

--- handler_refresh.go
+++ handler_refresh.go
@@ -3,51 +3,54 @@ package main
 import (
 	"net/http"
 	"time"
-
 	"github.com/e-300/http-server-go/internal/auth"
 
 )
 
 func (cfg *apiConfig) handlerRefresh(w http.ResponseWriter, r *http.Request){
-	//defer r.Body.Close()
 
 	type response struct{
 		Token string `json:"token"`
 	}
 
-	token, err := auth.GetBearerToken(r.Header)
+	refreshToken, err := auth.GetBearerToken(r.Header)
 	if err != nil{
-		respondWithError(w, 401, "Token Missing From Header", err)
+		respondWithError(w, 401, "Couldnt find Token", err)
 		return
 	}
 
-	dbToken, err := cfg.db.GetRefreshToken(r.Context(), token)
+	user, err := cfg.db.GetUserFromRefreshToken(r.Context(), refreshToken)
 	if err != nil{
 		respondWithError(w, 401, "Token Not in DB", err)
 		return
 	}
 
-	// token is expired???
-	if time.Now().After(dbToken.ExpiresAt){
-		respondWithError(w, 401, "Token is Expired", err)
-		return
-	}
-
-	// token is revoked??
-	if dbToken.RevokedAt.Valid == true{
-		respondWithError(w, 401, "Token is Revoked", err)
-		return
-	}
+//	// token is expired???
+//	if time.Now().After(dbToken.ExpiresAt){
+//		respondWithError(w, 401, "Token is Expired", err)
+//		return
+//	}
+//
+//	// token is revoked??
+//	if dbToken.RevokedAt.Valid == true{
+//		respondWithError(w, 401, "Token is Revoked", err)
+//		return
+//	}
 	// issue new jwt
-	jwt, err := auth.MakeJWT(dbToken.UserID, cfg.token_string, time.Hour)
+	accessToken, err := auth.MakeJWT(
+		user.ID, 
+		cfg.token_string, 
+		time.Hour,
+	)
+
 	if err != nil{
-		respondWithError(w, 401, "New jwt could not be issued", err)
+		respondWithError(w, 401, "Couldnt issue access Token", err)
 		return
 	}
 
 
 	respondWithJSON(w, 200, response{
-		Token: jwt,
+		Token: accessToken,
 	})
 
 }
--- handler_user_login.go
+++ handler_user_login.go
@@ -12,74 +12,79 @@ import (
 	//"github.com/e-300/http-server-go/internal/database"
 )
 
+func (cfg *apiConfig) handlerUserLogin(w http.ResponseWriter, r *http.Request) {
+	//defer r.Body.Close()
 
-func (cfg *apiConfig) handlerUserLogin(w http.ResponseWriter, r *http.Request){
-	defer r.Body.Close()
-
-	type parameters struct{
-		Email string 			  `json:"email"`
-		Password  string 	      `json:"password"`
-		// Token			string 	  `json:"token"`
-		// RefreshToken	string    `json:"refresh_token"`
-
+	type parameters struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
 	}
+
 	type response struct {
 		User
+		Token        string `json:"token"`
+		RefreshToken string `json:"refresh_token"`
 	}
 
 	decoder := json.NewDecoder(r.Body)
 	params := parameters{}
 	err := decoder.Decode(&params)
 	if err != nil {
-		respondWithError(w, 401, "Couldn't decode parameters", err)
+		respondWithError(w, http.StatusInternalServerError, "Couldn't decode parameters", err)
 		return
 	}
 
 	user, err := cfg.db.GetUser(r.Context(), params.Email)
-	if err != nil{
+	if err != nil {
 		respondWithError(w, 401, "Incorrect email or password", err)
 		return
 
 	}
 
-	match , err := auth.CheckPasswordHash(params.Password, user.HashedPassword)
-	if err != nil || !match{
+	match, err := auth.CheckPasswordHash(params.Password, user.HashedPassword)
+	if err != nil || !match {
 		respondWithError(w, 401, "Incorrect email or password", err)
-		return		
+		return
 	}
 
-	
-	signedAccessToken, err := auth.MakeJWT(user.ID, cfg.token_string, time.Hour)
-	if err != nil{
+	accessToken, err := auth.MakeJWT(
+		user.ID,
+		cfg.token_string,
+		time.Hour,
+	)
+	if err != nil {
 		respondWithError(w, 401, "Access Token Signing Issue", err)
 		return
 	}
 
 	// creating new refresh token
 	refreshToken, err := auth.MakeRefreshToken()
-	if err != nil{
-		respondWithError(w, 401, "Access Token Signing Issue", err)
+	if err != nil {
+		respondWithError(w, 401, "Couldnt create access JWT", err)
 		return
 	}
 
 	_, err = cfg.db.CreateRefresh(r.Context(), database.CreateRefreshParams{
-		Token: refreshToken,
-		UserID: user.ID, 
+		Token:     refreshToken,
+		UserID:    user.ID,
+		ExpiresAt: time.Now().UTC().Add(time.Hour * 24 * 60),
 	})
-	if err != nil{
-		respondWithError(w, 401, "Refresh Token could not be inserted into DB", err)
+
+	if err != nil {
+		respondWithError(w, 401, "Couldnt save refresh token", err)
 		return
 	}
 
-
+	
 	respondWithJSON(w, 200, response{
-		User{
-			ID: user.ID,
+		User: User{
+			ID:        user.ID,
 			CreatedAt: user.CreatedAt,
 			UpdatedAt: user.UpdatedAt,
-			Email: user.Email,
-			Token: signedAccessToken,
-			RefreshToken: refreshToken,
-	}})
+			Email:     user.Email,
+		},
 
+		Token:        accessToken,
+		RefreshToken: refreshToken,
+	})
 }
