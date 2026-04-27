c0700f0d043ec5c8e0d082023403054e89db1829
Ebad S <ebad@ebad.dev>
2026-04-26 23:06:39 -0500

    user based auth completed

--- handler_user_update_login.go
+++ handler_user_update_login.go
@@ -1,18 +1,24 @@
 package main
 
 import (
-	"net/http"
 	"encoding/json"
+	"net/http"
+
 	"github.com/e-300/http-server-go/internal/auth"
+	"github.com/e-300/http-server-go/internal/database"
 )
 
 func (cfg *apiConfig) handlerUserUpdateLogin(w http.ResponseWriter, r *http.Request){
 	defer r.Body.Close()
 	
 	type parameters struct{
-		Token		string	`json:"token"`
+//		Token		string	`json:"token"`
 		Email		string	`json:"email"`
 		Password	string	`json:"password"`	
+	}
+
+	type response struct{
+		User
 	}
 
 	decoder := json.NewDecoder(r.Body)
@@ -23,24 +29,51 @@ func (cfg *apiConfig) handlerUserUpdateLogin(w http.ResponseWriter, r *http.Requ
 		return
 	}
 	
-	refreshToken, err := auth.GetBearerToken(r.Header)
+	accessToken, err := auth.GetBearerToken(r.Header)
 	if err != nil{
 		respondWithError(w, 401, "Couldnt find Token", err)
 		return
 	}
 
-	user, err := cfg.db.GetUserFromRefreshToken(r.Context(), refreshToken)
+	user, err := auth.ValidateJWT(accessToken,cfg.token_string) 
 	if err != nil{
-		respondWithError(w, 401, "Token Not in DB", err)
+		respondWithError(w, 401, "jwt not found", err)
 		return
 	}
 
-	// create sql query to update email and password of the authenticated user
-	// hash new password from the request body then update password 
-	// errors -> respond with 401
-	// everything goes well respond with 200 code and with the newly updated user 
-		// omit password in the response
+	
+	
+	//Unnessary call can probably get rid of this
+	dbUser, err := cfg.db.GetUserFromId(r.Context(), user)
+	if err != nil{
+		respondWithError(w, 401, "user could not be found in db", err)
+		return
+	}
+	
+	hashedPassword, err := auth.HashPassword(params.Password)
+	if err != nil{
+		respondWithError(w, 401, "Password couldt not be hashed", err)
+		return
+	}
 
+	newCreds := database.UpdateEmailAndPasswordParams{
+		ID: dbUser.ID,
+		Email: params.Email,
+		HashedPassword: hashedPassword,
+	}
 
+	updatedUser, err := cfg.db.UpdateEmailAndPassword(r.Context(), newCreds)
+	if err != nil {
+		respondWithError(w, 401, "Couldnt not update to New Creds", err)
+		return
+	}
 
+	respondWithJSON(w, 200, response{ 
+		User:User{
+			ID: updatedUser.ID,
+			CreatedAt: updatedUser.CreatedAt,
+			UpdatedAt: updatedUser.UpdatedAt,
+			Email: updatedUser.Email,
+			},
+	})
 }
