321dc6808b75f269976eb9a82a2897376c4b6caa
Ebad S <ebad@ebad.dev>
2026-05-01 20:02:09 -0500

    just saving progress dawg, gonna finish this func today

--- handler_chirps_get_all.go
+++ handler_chirps_get_all.go
@@ -1,28 +1,37 @@
 package main
 
 import (
-
 	"net/http"
 )
 
-func (cfg *apiConfig) handlerChirpsAll(w http.ResponseWriter, r *http.Request){
+func (cfg *apiConfig) handlerChirpsAll(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
-	
+	authorID := r.URL.Query().Get("author_id")
+	if authorID == "" {
+		authorID = "Default"
+	}
+
+	// TO DOOO
+	// gotta write a sql query to sort chirps based on author id
+	//	by created at in ascedning order
+	// call that function if authorID != Default
+	// then return the sorted list of chirps
+
 	dbChirps, err := cfg.db.GetAllChirps(r.Context())
-	if err != nil{
+	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Couldn't get all chirps from database /ncomon mayn", err)
 		return
 	}
 
 	chirps := []Chirp{}
 
-	for _, chirp := range(dbChirps){
-		chirps = append(chirps,Chirp{
-			ID: chirp.ID,
+	for _, chirp := range dbChirps {
+		chirps = append(chirps, Chirp{
+			ID:        chirp.ID,
 			CreatedAt: chirp.CreatedAt,
-			UpdatedAt: chirp.UpdatedAt,	
-			Body: chirp.Body,     	
+			UpdatedAt: chirp.UpdatedAt,
+			Body:      chirp.Body,
 			//UserID: chirp.UserID,
 		})
 	}
